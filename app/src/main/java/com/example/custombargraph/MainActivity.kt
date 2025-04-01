ae946eb4c55ceed4deb3b5b44c77e6701b771caa
Dineshkumar Nithra <71814124+dineshkumar-my-apps@users.noreply.github.com>
2025-04-01 12:25:17 +0530

    Custom Bar Graph Code Push

--- app/src/main/java/com/example/custombargraph/MainActivity.kt
+++ app/src/main/java/com/example/custombargraph/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
@@ -13,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.custombargraph.ui.theme.CustomBarGraphTheme
@@ -32,15 +34,27 @@ class MainActivity : ComponentActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
-                    val dataList = mutableListOf(30,60,90,50,70)
+                    val dataList = mutableListOf(120,40,50,10,80,90,100)
                     val floatValue = mutableListOf<Float>()
-                    val datesList = mutableListOf(2,3,4,5,6)
+                    val datesList = mutableListOf(1,2,3,4,5,6,7)
 
                     dataList.forEachIndexed { index, value ->
 
                         floatValue.add(index = index, element = value.toFloat()/dataList.max().toFloat())
 
                     }
+
+                    Text(
+                        text = "Custom Bar Graph",
+                        color = Color.Black,
+                        style = MaterialTheme.typography.h5,
+                        modifier = Modifier.padding(bottom = 20.dp),
+                        fontWeight = MaterialTheme.typography.h5.fontWeight,
+                        fontSize = MaterialTheme.typography.h5.fontSize,
+                        textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                    )
+
+                    Spacer(modifier = Modifier.padding(20.dp))
 
                     BarGraph(
                         graphBarData = floatValue,
@@ -49,7 +63,7 @@ class MainActivity : ComponentActivity() {
                         height = 300.dp,
                         roundType = BarType.TOP_CURVED,
                         barWidth = 20.dp,
-                        barColor = Purple500,
+                        barColor = Color.Blue,
                         barArrangement = Arrangement.SpaceEvenly
                     )
                 }
